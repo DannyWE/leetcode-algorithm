e55ecc408a0396c2805c25643a24b2a4fcd27569
Xiaohe Dong <danny.dong@rea-group.com>
2017-09-10 15:51:28 +1000

    Commit an exmaple with not tail recursive exmaple

--- src/main/scala/algorithm/SameTree.scala
+++ src/main/scala/algorithm/SameTree.scala
@@ -1,5 +1,7 @@
 package algorithm
 
+import scala.annotation.tailrec
+
 case class TreeNode(_value: Int,
                     left: Option[TreeNode],
                     right: Option[TreeNode]
@@ -7,7 +9,18 @@ case class TreeNode(_value: Int,
 
 object SameTree {
 
-  def apply(p: TreeNode, q: TreeNode): Boolean = {
-    ???
+  def apply(p: TreeNode, q: TreeNode): Boolean = compareTo(p, q)
+
+  def compareTo(p: TreeNode, q: TreeNode): Boolean = {
+    (p, q) match {
+      case (TreeNode(value1, None, None), TreeNode(value2, None, None)) if value1 == value2 => true
+      case (TreeNode(value1, Some(t), None),
+      TreeNode(value2, Some(r), None)) if value1 == value2 => compareTo(t, r)
+      case (TreeNode(value1, None, Some(t)),
+      TreeNode(value2, None, Some(r))) if value1 == value2 => compareTo(t, r)
+      case (TreeNode(value1, Some(left1), Some(right1)),
+      TreeNode(value2, Some(left2), Some(right2))) if value1 == value2 => compareTo(left1, left2) && compareTo(right1, right2)
+      case _ => false
+  }
   }
 }
--- src/test/scala/algorithm/SameTreeTest.scala
+++ src/test/scala/algorithm/SameTreeTest.scala
@@ -5,9 +5,20 @@ import infrastructure.BaseTableDrivenTest
 class SameTreeTest extends BaseTableDrivenTest {
 
   val table = Table(
-    ("Tree",                      "Tree to Compare",                    "Expected Result"),
+    ("Tree",                                               "Tree to Compare",                                             "Expected Result"),
 
-    (TreeNode(3, None, None),     TreeNode(3, None, None),               true)
+    (TreeNode(3, None, None),                              TreeNode(3, None, None),                                        true),
+    (TreeNode(3, None, None),                              TreeNode(4, None, None),                                        false),
+    (TreeNode(3, None, None),                              TreeNode(4, Some(TreeNode(3, None, None)), None),               false),
+    (TreeNode(3, Some(TreeNode(3, None, None)), None),     TreeNode(3, Some(TreeNode(3, None, None)), None),               true),
+    (TreeNode(3, Some(TreeNode(3, None, None)),
+      Some(TreeNode(3, None, None))),                      TreeNode(3, Some(TreeNode(3, None, None)), None),               false),
+    (TreeNode(3, Some(TreeNode(3, None, None)),
+      Some(TreeNode(3, None, None))),                      TreeNode(3, Some(TreeNode(3, None, None)),
+                                                           Some(TreeNode(7, None, None))),                                 false),
+    (TreeNode(3, Some(TreeNode(3, None, None)),
+      Some(TreeNode(3, None, None))),                      TreeNode(3, Some(TreeNode(3, None, None)),
+                                                           Some(TreeNode(3, None, None))),                                 true),
   )
 
   test("should ") {
