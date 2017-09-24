21f618c914a190bddd1d16a5edd40d99b8b622b5
Xiaohe Dong <danny.dong@rea-group.com>
2017-09-24 21:31:04 +1000

    Palindrome example

---
+++ src/main/scala/algorithm/PalindromeNumber.scala
@@ -0,0 +1,15 @@
+package algorithm
+
+//https://leetcode.com/problems/palindrome-number/description/
+
+object PalindromeNumber {
+
+  def apply(x: Int): Boolean = {
+    val strX = x.toString
+    val length = strX.length
+    val half = length / 2
+    val last = length - 1
+
+    (0 to half).forall(i => strX(i).equals(strX(last - i)))
+  }
+}
---
+++ src/test/scala/algorithm/PalindromeNumberTest.scala
@@ -0,0 +1,20 @@
+package algorithm
+
+import infrastructure.BaseTableDrivenTest
+
+class PalindromeNumberTest extends BaseTableDrivenTest {
+
+  val table = Table(
+    ("Number",                     "Expected"),
+    (123,                           false),
+    (151,                           true),
+    (3,                             true)
+  )
+
+  test("should ") {
+    forAll(table)((number: Int, expected: Boolean) => {
+      PalindromeNumber(number) should equal(expected)
+    })
+  }
+}
+
