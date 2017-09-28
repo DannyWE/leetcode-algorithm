1773a50c45facd39edf1c4ecfaa206355350a80d
Xiaohe Dong <danny.dong@rea-group.com>
2017-09-28 23:56:01 +1000

    Add tree preOrder example

---
+++ src/main/scala/algorithm/tree/PreOrderTree.java
@@ -0,0 +1,42 @@
+package algorithm.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+
+public class PreOrderTree {
+
+    void preOrder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        preOrderConcat(root, list);
+        String result = "";
+        for (Integer ele : list) {
+            result += ele + " ";
+        }
+        System.out.println(result.trim());
+//        Stream<String> rStream = list.stream().map(Object::toString);
+//        System.out.println(String.join(" ",  rStream.collect(Collectors.toList())));
+    }
+
+    void preOrderConcat(Node root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.data);
+        if (root.left != null && root.right != null) {
+            preOrderConcat(root.left, list);
+            preOrderConcat(root.right, list);
+        } else if (root.left != null) {
+            preOrderConcat(root.left, list);
+        } else {
+            preOrderConcat(root.right, list);
+        }
+    }
+
+}
