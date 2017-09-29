e00e879d3bf78485f6690f1cfbc4cd53ab31cbe0
Xiaohe Dong <danny.dong@rea-group.com>
2017-09-29 20:21:30 +1000

    Add example fro Self balancing tree and priority queue test

---
+++ src/main/scala/algorithm/tree/PriorityQueueTest.java
@@ -0,0 +1,32 @@
+package algorithm.tree;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class PriorityQueueTest {
+
+    public static void main(String[] args) {
+        Comparator<Integer> comparator = Comparator.comparingInt(o -> o);
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(comparator);
+        int Q = sc.nextInt(); // the number of queries
+        int queryType = 0;
+        int element = 0;
+        while(Q-->0) {
+            queryType = sc.nextInt();
+            switch(queryType){
+                case 1:
+                    element = sc.nextInt();
+                    minHeap.add(element);
+                    break;
+                case 2:
+                    element = sc.nextInt();
+                    minHeap.remove(element);
+                    break;
+                case 3:
+                    System.out.println(minHeap.peek());
+            }
+        }
+    }
+}
