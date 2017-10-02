400ed5dd3ad78b453177cdf3ad4f399074c3f05f
Xiaohe Dong <danny.dong@rea-group.com>
2017-10-02 17:08:33 +1100

    Add merge sort and heap sort algorithm

---
+++ src/main/scala/algorithm/sorting/MergeSort.js
@@ -0,0 +1,32 @@
+mergeSort = (arr) => {
+    if (arr.length < 2) {
+        return arr;
+    }
+    const middle = parseInt(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+};
+
+merge = (left , right) => {
+    let result = [];
+
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length) {
+        result.push(left.shift());
+    }
+
+    while (right.length) {
+        result.push(right.shift());
+    }
+
+    return result;
+};
