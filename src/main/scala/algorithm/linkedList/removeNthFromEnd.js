220698ebaaaa4442e834e43297e572d473586678
Xiaohe Dong <danny.dong@rea-group.com>
2017-10-02 12:12:58 +1100

    Add Remove Nth Node From End of List

---
+++ src/main/scala/algorithm/linkedList/removeNthFromEnd.js
@@ -0,0 +1,42 @@
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+a = new ListNode(1);
+b = new ListNode(2);
+c = new ListNode(3);
+d = new ListNode(4);
+e = new ListNode(5);
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+size = (head) => {
+    let size = 1;
+    while(head.next !== null) {
+        ++ size;
+        head = head.next;
+    }
+    return size;
+};
+
+remove = (head, n) => {
+    const position = size(head) - n;
+    let index = 0;
+    while(index < position) {
+        if (index === position - 1) {
+            head.next = head.next.next !== null ? head.next.next : null;
+            break;
+        }
+        head = head.next;
+        ++ index;
+    }
+}
+
+removeNthFromEnd = (head, n) => {
+   remove(head, n);
+   return head;
+};
+
