b0f610f9a5e220d2d38167581c00f7910499c859
Xiaohe Dong <danny.dong@rea-group.com>
2017-10-12 11:17:33 +1100

    Update solution for Find Minimum in Rotated Sorted Array https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/

--- src/main/scala/algorithm/binarySearch/FindMinimuminRotatedSortedArray.js
+++ src/main/scala/algorithm/binarySearch/FindMinimuminRotatedSortedArray.js
@@ -1,11 +1,11 @@
 /*
 https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 
-findMinimum([4,5,6,7,0, 1,2])
+findMinimum([4,5,6,7,0,1,2])
 0
-findMinimum([4,5,6,7,0, 1,2, 3])
+findMinimum([4,5,6,7,0,1,2,3])
 0
-findMinimum([4,5,6,7,8,9,0, 1,2, 3])
+findMinimum([4,5,6,7,8,9,0,1,2,3])
 0
  */
 
@@ -14,43 +14,16 @@ findMinimum = (arr) => {
     let start = 0;
     let end = len - 1;
 
-    let fistTime = true;
-    while (start <= end) {
-        if (end - start === 1) {
-            return arr[start] < arr[end] ? arr[start] : arr[end];
-        }
-        if (start === end ) {
-            return arr[start];
-        }
-
+    while (start < end) {
         let mid = Math.floor((start + end) / 2);
 
-        if (arr[mid - 1] > arr[mid] && arr[mid + 1] > arr[mid] ) {
-            return arr[mid];
+        if (arr[mid] < arr[start]) {
+            end = mid;
+        } else if (arr[mid] > arr[end]) {
+            start = mid + 1;
+        } else {
+            return arr[start]
         }
-        if (fistTime) {
-            let i = mid;
-            let j = mid;
-            while (i > 0 || j < end) {
-                if (arr[i] > arr[i - 1]) {
-                    i --;
-                } else {
-                    end = mid - 1;
-                    fistTime = false;
-                    break;
-                }
-                if (arr[j] < arr[j + 1]) {
-                    j ++;
-                } else {
-                    start = mid + 1;
-                    fistTime = false;
-                    break;
-                }
-            }
-            continue;
-        }
-
-        arr[mid - 1] < arr[mid + 1] ? end = mid - 1 : start = mid + 1;
     }
-
+    return arr[start];
 };
